01e574c7dc29186bfa2622f465d754d44a93d97d
Choirul Anam <choirulanm.15@gmail.com>
2026-02-02 23:06:25 +0700

    fixed bug and query parameter

--- backend/notification-service/internal/helper/calculation.go
+++ backend/notification-service/internal/helper/calculation.go
@@ -1,9 +1,9 @@
 package helper
 
-func CalculateOffset(page, limit int32) *int32 {
+func CalculateOffset(page, limit int32) int32 {
 	if page < 1 {
 		page = 1
 	}
 	offset := (page - 1) * limit
-	return &offset
+	return offset
 }
